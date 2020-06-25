03e865448f3a68cb17c8a30e34df0d8b18497cac
FirefoxMetzger <sebastian@wallkoetter.net>
2020-06-25 09:27:59 +0200

    separated game logic and gym environment

--- minihex/HexGame.py
+++ minihex/HexGame.py
@@ -16,6 +16,126 @@ def empty_tiles(board):
     return idx
 
 
+class HexGame(object):
+    """
+    Hex Game Environment.
+    """
+
+    def __init__(self, active_player, board, player):
+        self.board = board
+
+        self.front = {player.WHITE: set((y, x)
+                                        for y in range(self.board_size)
+                                        for x in range(2)),
+                      player.BLACK: set((y, x)
+                                        for y in range(2)
+                                        for x in range(self.board_size))}
+        self.active_player = active_player
+        self.player = player
+        self.done = False
+
+    @property
+    def board_size(self):
+        return self.board.shape[1]
+
+    def is_reisgn_move(self, action):
+        return action == self.board.shape[1] ** 2
+
+    def is_valid_move(self, action):
+        coords = self.action_to_coordinate(action)
+        if self.board[2, coords[0], coords[1]] == 1:
+            return True
+        else:
+            return False
+
+    def make_move(self, action):
+        if self.is_reisgn_move(action):
+            self.done = True
+            return (self.active_player + 1) % 2
+
+        if not self.is_valid_move(action):
+            raise Exception(("Illegal move "
+                             f"{self.action_to_coordinate(action)}"))
+
+        coords = self.action_to_coordinate(action)
+        self.board[(2, *coords)] = 0
+        self.board[(self.active_player, *coords)] = 1
+
+        winner = self.update_front(action)
+        self.active_player = (self.active_player + 1) % 2
+        return winner
+
+    def coordinate_to_action(self, coords):
+        return np.ravel_multi_index(coords, (self.board_size, self.board_size))
+
+    def action_to_coordinate(self, action):
+        return np.unravel_index(action, (self.board_size, self.board_size))
+
+    def get_possible_actions(self):
+        coords = np.where(self.board[2, ...] == 1)
+        return self.coordinate_to_action(coords)
+
+    def update_front(self, action):
+        active_player = self.active_player
+        connections = np.array([[-1, -1,  0,  0,  1,  1],
+                                [0,   1, -1,  1, -1,  0]])
+
+        position = np.array(self.action_to_coordinate(action))
+
+        positions_to_test = [position]
+        while len(positions_to_test) > 0:
+            current_position = positions_to_test.pop()
+
+            if self.board[(active_player, *position)] == 0:
+                continue
+
+            if tuple(current_position) in self.front[active_player]:
+                continue
+
+            neighbours = list()
+            check_neighbours = False
+            for direction in connections.T:
+                neighbour_position = current_position + direction
+
+                if neighbour_position[0] >= self.board_size:
+                    if active_player == player.WHITE:
+                        self.done = True
+                        return player.WHITE
+                    else:
+                        continue
+
+                if neighbour_position[1] >= self.board_size:
+                    if active_player == player.BLACK:
+                        self.done = True
+                        return player.BLACK
+                    else:
+                        continue
+
+                if np.any(neighbour_position < 0):
+                    continue
+
+                if self.board[(active_player, *neighbour_position)] == 1:
+                    neighbours.append(neighbour_position)
+
+                if tuple(neighbour_position) in self.front[active_player]:
+                    self.front[active_player].add(tuple(current_position))
+                    check_neighbours = True
+
+            if check_neighbours:
+                positions_to_test += neighbours
+
+        if np.all(self.board[2, :, :] == 0):
+            self.done = True
+        return None
+
+    # function for introspection
+    def front_array(self, player):
+        front_array = np.zeros((self.board_size, self.board_size))
+        for pos in self.front[player]:
+            front_array[pos] = 1.0
+        return front_array
+
+
 class HexEnv(gym.Env):
     """
     Hex environment. Play against a fixed opponent.
@@ -25,59 +145,60 @@ class HexEnv(gym.Env):
 
     def __init__(self, opponent_policy,
                  player_color=player.BLACK,
+                 active_player=player.BLACK,
+                 board=None,
                  board_size=5):
-
-        self.board_size = board_size+4
-        self.playable_board_size = board_size
-        self.player_color = player_color
         self.opponent_policy = opponent_policy
+        self.action_space = spaces.Discrete(board_size ** 2 + 1)
 
-        self.action_space = spaces.Discrete(self.board_size ** 2 + 1)
+        if board is None:
+            board = np.zeros((3, board_size+4, board_size+4))
+            board[player.BLACK, :2, :] = 1
+            board[player.BLACK, -2:, :] = 1
+            board[player.WHITE, :, :2] = 1
+            board[player.WHITE, :, -2:] = 1
+            board[2, 2:-2, 2:-2] = 1
 
-        self.board = np.zeros((3, self.board_size, self.board_size))
-        self.move_count = 0
-        self.active_player = player.BLACK
-        self.done = False
+        self.initial_board = board
+        self.active_player = active_player
+        self.player = player_color
+        self.simulator = None
+        self.winner = None
 
-        # speed optimization
-        self.front = {player.BLACK: set(), player.WHITE: set()}
+    @property
+    def opponent(self):
+        return (self.player + 1) % 2
 
     def reset(self):
-        self.board = np.zeros((3, self.board_size, self.board_size))
-        self.board[player.BLACK, :2, :] = 1
-        self.board[player.BLACK, -2:, :] = 1
-        self.board[player.WHITE, :, :2] = 1
-        self.board[player.WHITE, :, -2:] = 1
-        self.board[2, 2:-2, 2:-2] = 1
+        self.simulator = HexGame(self.active_player,
+                                 self.initial_board.copy(),
+                                 self.player)
 
-        self.front = {player.BLACK: set(), player.WHITE: set()}
-        self.front[player.BLACK] = set((y, x)
-                                       for x in range(self.board_size)
-                                       for y in [0, 1])
-        self.front[player.WHITE] = set((y, x)
-                                       for x in [0, 1]
-                                       for y in range(self.board_size))
-
-        self.active_player = player.BLACK
-        self.done = False
-
-        # Let the opponent play if it's not the agent's turn
-        if self.player_color != self.active_player:
+        if self.player != self.active_player:
             self.opponent_move()
 
-        return self.board
+        return self.simulator.board
 
     def step(self, action):
-        reward = self.make_move(action)
+        if not self.simulator.done:
+            self.winner = self.simulator.make_move(action)
 
-        if not self.done:
-            action = self.opponent_policy(self.board)
-            reward = self.make_move(action)
+        if not self.simulator.done:
+            action = self.opponent_policy(self.simulator.board)
+            self.winner = self.simulator.make_move(action)
 
-        return self.board, reward, self.done, {'state': self.board}
+        if self.winner == self.player:
+            reward = 1
+        elif self.winner == self.opponent:
+            reward = -1
+        else:
+            reward = 0
+
+        return (self.simulator.board, reward,
+                self.simulator.done, {'state': self.simulator.board})
 
     def render(self, mode='ascii', close=False):
-        board = self.board
+        board = self.simulator.board[:, 2:-2, 2:-2]
         print(" " * 6, end="")
         for j in range(board.shape[1]):
             print(" ", j + 1, " ", end="")
@@ -101,103 +222,3 @@ class HexEnv(gym.Env):
             print(" " * (i * 3 + 1), end="")
             print("-" * (board.shape[1] * 7 - 1), end="")
             print("")
-
-    def is_reisgn_move(self, action):
-        return action == self.board_size ** 2
-
-    def is_valid_move(self, action):
-        coords = self.action_to_coordinate(action)
-        if self.board[2, coords[0], coords[1]] == 1:
-            return True
-        else:
-            return False
-
-    def make_move(self, action):
-        if self.is_reisgn_move(action):
-            self.done = True
-            if self.active_player == player.BLACK:
-                return self.board, 1, True, {'state': self.board}
-            else:
-                return self.board, -1, True, {'state': self.board}
-
-        if not self.is_valid_move(action):
-            if self.active_player != self.player_color:
-                raise Exception(("Opponent policy played illegal move"
-                                 f" {self.action_to_coordinate(action)}"))
-            else:
-                raise Exception(("Illegal move "
-                                 f"{self.action_to_coordinate(action)}"))
-
-        coords = self.action_to_coordinate(action)
-        self.board[(2, *coords)] = 0
-        self.board[(self.active_player, *coords)] = 1
-
-        reward = self.game_finished(action)
-        self.active_player = (self.active_player + 1) % 2
-        return reward
-
-    def coordinate_to_action(self, coords):
-        return np.ravel_multi_index(coords, (self.board_size, self.board_size))
-
-    def action_to_coordinate(self, action):
-        return np.unravel_index(action, (self.board_size, self.board_size))
-
-    def get_possible_actions(self):
-        coords = np.where(self.board[2, ...] == 1)
-        return self.coordinate_to_action(coords)
-
-    def game_finished(self, action):
-        active_player = self.active_player
-        connections = np.array([[-1, -1,  0,  0,  1,  1],
-                                [0,   1, -1,  1, -1,  0]])
-
-        position = np.array(self.action_to_coordinate(action))
-
-        positions_to_test = [position]
-        while len(positions_to_test) > 0:
-            current_position = positions_to_test.pop()
-
-            if tuple(current_position) in self.front[active_player]:
-                continue
-
-            neighbours = list()
-            check_neighbours = False
-            for direction in connections.T:
-                neighbour_position = current_position + direction
-
-                if neighbour_position[0] >= self.board_size:
-                    if active_player == player.WHITE:
-                        self.done = True
-                        return 1
-                    else:
-                        continue
-
-                if neighbour_position[1] >= self.board_size:
-                    if active_player == player.BLACK:
-                        self.done = True
-                        return -1
-                    else:
-                        continue
-
-                if np.any(neighbour_position < 0):
-                    continue
-
-                if self.board[(active_player, *neighbour_position)] == 1:
-                    neighbours.append(neighbour_position)
-
-                if tuple(neighbour_position) in self.front[active_player]:
-                    self.front[active_player].add(tuple(current_position))
-                    check_neighbours = True
-
-            if check_neighbours:
-                positions_to_test += neighbours
-
-        if np.all(self.board[2, :, :] == 0):
-            self.done = True
-        return 0
-
-    def front_array(self, player):
-        front_array = np.zeros((self.board_size, self.board_size))
-        for pos in self.front[player]:
-            front_array[pos] = 1.0
-        return front_array
