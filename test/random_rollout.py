95e0ac1edd48f2d886b98c8b99b8568cf680600f
FirefoxMetzger <sebastian@wallkoetter.net>
2020-07-01 19:14:16 +0200

    updated speed test

--- test/random_rollout.py
+++ test/random_rollout.py
@@ -6,14 +6,12 @@ env = gym.make("hex-v0",
                opponent_policy=minihex.random_policy,
                board_size=11)
 
-import tqdm
-for _ in tqdm.tqdm(range(1000)):
-    state, info = env.reset()
-    done = False
-    while not done:
-        board, player = state
-        action = minihex.random_policy(board, player, info)
-        state, reward, done, info = env.step(action)
+state, info = env.reset()
+done = False
+while not done:
+    board, player = state
+    action = minihex.random_policy(board, player, info)
+    state, reward, done, info = env.step(action)
 
 env.render()
 
